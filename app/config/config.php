d4e34118164a5ef288bc5b556319124c39ae55db
Jacquet Nathan <jacquet.nathan2@gmail.com>
2020-10-01 20:26:51 +0200

    ajout

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+    // DB Params
+    define('DB_HOST', 'us-cdbr-east-02.cleardb.com');
+    define('DB_USER', 'b6dd2d20d46ff3');
+    define('DB_PASS', '18225815');
+    define('DB_NAME', 'heroku_6b70354f618394f');
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost/COGIP-app');
+
+    // Site Name
+    define('SITENAME', 'Cogip');
+
+    // App Version
+    define('APPVERSION', '1.0.0');
